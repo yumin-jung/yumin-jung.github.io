14ab87bea13d1dfbea8169ee8f61cb7ae84a864b
yumin-jung <jungym887@naver.com>
2022-05-03 16:30:39 +0900

    Init blog 🚀

---
+++ src/App.svelte
@@ -0,0 +1,32 @@
+<script>
+	export let name;
+</script>
+
+<main>
+	<h1>Hi!</h1>
+	<p>
+		{name}'s blog
+	</p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #505050;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
